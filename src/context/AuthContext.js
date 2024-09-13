42027683ff127c6676fadbb79e8c0c7f623ff2e7
Nirosh Madushan <127856472+niroshmadushan@users.noreply.github.com>
2024-09-13 15:07:19 +0530

    jksfhkdfj

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -12,7 +12,7 @@ export const AuthProvider = ({ children }) => {
 
   const login = async (email, password) => {
     try {
-      const response = await axios.post('login.php', { email, password });
+      const response = await axios.post('https://appservice.wuaze.com/login.php', { email, password });
       const userData = response.data.user;
   console.log(userData);
       if (!userData || !userData.role) {
