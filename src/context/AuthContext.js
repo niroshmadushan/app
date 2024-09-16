69366fda62278b73bc231afc86ce5c7c5f8bb16d
Nirosh Madushan <127856472+niroshmadushan@users.noreply.github.com>
2024-09-16 20:34:21 +0530

    zdfdfd

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -12,7 +12,7 @@ export const AuthProvider = ({ children }) => {
 
   const login = async (email, password) => {
     try {
-      const response = await fetch('http://appservice.wuaze.com/login.php', {
+      const response = await fetch('login.php', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
